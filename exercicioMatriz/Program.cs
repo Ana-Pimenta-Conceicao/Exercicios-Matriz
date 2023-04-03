82a97825abd3002458f8070897143f1a66d997e9
Ana-Pimenta-Conceicao <ana.pimenta120505@gmail.com>
2023-04-03 13:11:59 -0300

    Primeiro Commit

---
+++ exercicioMatriz/Program.cs
@@ -0,0 +1,12 @@
+﻿int[,] mat = new int[5, 5]; 
+int l, c, soma = 0;
+Console.WriteLine("Insira 9 valores para a matriz:"); 
+for (l = 0; l < 5; l++) { for (c = 0; c < 5; c++) {
+        Console.WriteLine("Informe o valor do indice " + l + " , " + c);
+        mat[l, c] = Convert.ToInt32(Console.ReadLine()); 
+    } }
+for (l = 0; l < 5; l++) { 
+    for (c = 0; c < 5; c++) 
+    { if (l < c) { 
+            Console.WriteLine("Elementos acima" + mat[l, c]); } 
+        if (l > c) { Console.WriteLine("Elementos abaixo" + mat[l, c]); } } }
